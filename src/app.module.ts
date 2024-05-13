e00db807355f109f3bed6f0b4a59b2adf0cb3256
Guiniel <gitllermo23@gmail.com>
2024-05-13 16:25:42 -0400

    Arreglando las variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,11 @@ import { SongSchema } from './song/entities/song.entity';
 import { SongService } from './song/song.service';
 import { SongController } from './song/song.controller';
 
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 @Module({
   
   imports: [
     ConfigModule.forRoot({isGlobal: true}),
-    MongooseModule.forRoot(uri, {dbName: 'songdb'}),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {dbName: 'songdb'}),
     MongooseModule.forFeature([{ name: 'Song', schema: SongSchema }]),
   ],
   controllers: [SongController],
