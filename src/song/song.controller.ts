694493e77b373744927e61108678ae35f4babff0
Guiniel <gitllermo23@gmail.com>
2024-05-13 22:51:41 -0400

    Agregando un seed para la base de datos

--- src/song/song.controller.ts
+++ src/song/song.controller.ts
@@ -25,6 +25,37 @@ export class SongController {
     }
   }
 
+  @Get('seed')
+  async poblateDatabase(@Res() response) {
+    try {
+      const songsToAdd = [
+        { name: 'Cancion 1', artist: 'Artista 1', duration: 100, genre: 'Pop' },
+        { name: 'Cancion 2', artist: 'Artista 2', duration: 200, genre: 'Rock' },
+        { name: 'Cancion 3', artist: 'Artista 3', duration: 300, genre: 'R&B' },
+        { name: 'Cancion 4', artist: 'Artista 4', duration: 5, genre: 'Hip Hop' },
+        { name: 'Cancion 5', artist: 'Artista 5', duration: 30, genre: 'Dance' },
+        { name: 'Cancion 6', artist: 'Artista 6', duration: 300, genre: 'Electronica' },
+        { name: 'Cancion 7', artist: 'Artista 7', duration: 300, genre: 'Salsa' },
+        { name: 'Cancion 8', artist: 'Artista 8', duration: 300, genre: 'Latino' },
+        { name: 'Cancion 9', artist: 'Artista 9', duration: 300, genre: 'Mariachi' },
+        { name: 'Cancion 10', artist: 'Artista 10', duration: 300, genre: 'Folk' }
+      ];
+  
+      const newSongs = await Promise.all(songsToAdd.map(song => this.songService.addSong(song)));
+  
+      return response.status(201).json({
+        message: 'Songs have been added to the database successfully',
+        newSongs,
+      });
+    } catch (err) {
+      return response.status(400).json({
+        statusCode: 400,
+        message: 'Error: Songs not added to the database!',
+        error: 'Bad Request',
+      });
+    }
+  }
+
   @Get()
   async getSongs(@Res() response) {
     try {
