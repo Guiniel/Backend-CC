d821c2f3dd05e5706251e68a8abd693cda15acdf
Guillermo <gitllermo23@gmail.com>
2024-06-07 14:59:28 -0400

    Commit para pull

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { SongController } from './song/song.controller';
   
   imports: [
     ConfigModule.forRoot({isGlobal: true}),
-    MongooseModule.forRoot(process.env.MONGODB_URI, {dbName: 'songdb'}),
+    MongooseModule.forRoot("mongodb://127.0.0.1:27017", {dbName: 'songdb'}),
     MongooseModule.forFeature([{ name: 'Song', schema: SongSchema }]),
   ],
   controllers: [SongController],
